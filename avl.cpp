26129133d1da034db1c0d8fe0ed40b64a87f0a97
Manalinarkhede <manalinarkhede27@gmail.com>
2025-05-04 22:54:37 +0530

    Update avl.cpp

--- avl.cpp
+++ avl.cpp
@@ -15,192 +15,165 @@ public:
     node* root;
     AVL() { root = NULL; }
 
-    node* insert(node*, string, string);
-    node* deleteNode(node*, string);
-    void preorder(node*);
-    void inorder(node*);
-    node* RotateRight(node*);
-    node* RotateLeft(node*);
-    node* RR(node*);
-    node* LL(node*);
-    node* LR(node*);
-    node* RL(node*);
-    int height(node*);
-    int BF(node*);
-    void search(node*, string);
-    void modify(node*, string);
-    node* findMin(node*);
+    int height(node* temp) {
+        if (temp == NULL)
+            return 0;
+        return 1 + max(height(temp->left), height(temp->right));
+    }
+
+    int BF(node* temp) {
+        if (temp == NULL)
+            return 0;
+        return height(temp->left) - height(temp->right);
+    }
+
+    node* RotateRight(node* parent) {
+        node* temp = parent->left;
+        parent->left = temp->right;
+        temp->right = parent;
+        parent->ht = height(parent);
+        temp->ht = height(temp);
+        return temp;
+    }
+
+    node* RotateLeft(node* parent) {
+        node* temp = parent->right;
+        parent->right = temp->left;
+        temp->left = parent;
+        parent->ht = height(parent);
+        temp->ht = height(temp);
+        return temp;
+    }
+
+    node* RR(node* T) {
+        return RotateLeft(T);
+    }
+
+    node* LL(node* T) {
+        return RotateRight(T);
+    }
+
+    node* LR(node* T) {
+        T->left = RotateLeft(T->left);
+        return RotateRight(T);
+    }
+
+    node* RL(node* T) {
+        T->right = RotateRight(T->right);
+        return RotateLeft(T);
+    }
+
+    node* insert(node* temp, string str_w, string str_m) {
+        if (temp == NULL) {
+            temp = new node;
+            temp->word = str_w;
+            temp->meaning = str_m;
+            temp->left = temp->right = NULL;
+        } else {
+            if (str_w.compare(temp->word) > 0) {
+                temp->right = insert(temp->right, str_w, str_m);
+                if (BF(temp) == -2) {
+                    if (temp->right && str_w.compare(temp->right->word) > 0)
+                        temp = RR(temp);
+                    else
+                        temp = RL(temp);
+                }
+            } else {
+                temp->left = insert(temp->left, str_w, str_m);
+                if (BF(temp) == 2) {
+                    if (temp->left && str_w.compare(temp->left->word) < 0)
+                        temp = LL(temp);
+                    else
+                        temp = LR(temp);
+                }
+            }
+        }
+        temp->ht = height(temp);
+        return temp;
+    }
+
+    node* findMin(node* root) {
+        while (root->left != NULL)
+            root = root->left;
+        return root;
+    }
+
+    node* deleteNode(node* root, string str_w) {
+        if (root == NULL)
+            return NULL;
+
+        if (str_w < root->word)
+            root->left = deleteNode(root->left, str_w);
+        else if (str_w > root->word)
+            root->right = deleteNode(root->right, str_w);
+        else {
+            if (root->left == NULL) {
+                node* temp = root->right;
+                delete root;
+                return temp;
+            } else if (root->right == NULL) {
+                node* temp = root->left;
+                delete root;
+                return temp;
+            } else {
+                node* temp = findMin(root->right);
+                root->word = temp->word;
+                root->meaning = temp->meaning;
+                root->right = deleteNode(root->right, temp->word);
+            }
+        }
+        root->ht = height(root);
+        return root;
+    }
+
+    void preorder(node* root) {
+        if (root != NULL) {
+            cout << root->word << " : " << root->meaning << " (Bf=" << BF(root) << ")\n";
+            preorder(root->left);
+            preorder(root->right);
+        }
+    }
+
+    void inorder(node* root) {
+        if (root != NULL) {
+            inorder(root->left);
+            cout << root->word << " : " << root->meaning << " (Bf=" << BF(root) << ")\n";
+            inorder(root->right);
+        }
+    }
+
+    void search(node* root, string str_w) {
+        if (root == NULL) {
+            cout << "Word not found" << endl;
+            return;
+        }
+
+        if (str_w.compare(root->word) < 0)
+            search(root->left, str_w);
+        else if (str_w.compare(root->word) > 0)
+            search(root->right, str_w);
+        else {
+            cout << "Word: " << root->word << "\nMeaning: " << root->meaning << endl;
+        }
+    }
+
+    void modify(node* root, string str_w) {
+        if (root == NULL) {
+            cout << "Word not found" << endl;
+            return;
+        }
+        if (str_w.compare(root->word) < 0)
+            modify(root->left, str_w);
+        else if (str_w.compare(root->word) > 0)
+            modify(root->right, str_w);
+        else {
+            cout << "Enter new meaning: ";
+            cin.ignore();
+            getline(cin, root->meaning);
+        }
+    }
 };
 
-// Calculate height
-int AVL::height(node* temp) {
-    if (temp == NULL)
-        return 0;
-    return 1 + max(height(temp->left), height(temp->right));
-}
-
-// Balance Factor
-int AVL::BF(node* temp) {
-    if (temp == NULL)
-        return 0;
-    return height(temp->left) - height(temp->right);
-}
-
-// Rotations
-node* AVL::RotateRight(node* parent) {
-    node* temp = parent->left;
-    parent->left = temp->right;
-    temp->right = parent;
-    parent->ht = height(parent);
-    temp->ht = height(temp);
-    return temp;
-}
-
-node* AVL::RotateLeft(node* parent) {
-    node* temp = parent->right;
-    parent->right = temp->left;
-    temp->left = parent;
-    parent->ht = height(parent);
-    temp->ht = height(temp);
-    return temp;
-}
-
-node* AVL::RR(node* T) {
-    return RotateLeft(T);
-}
-
-node* AVL::LL(node* T) {
-    return RotateRight(T);
-}
-
-node* AVL::LR(node* T) {
-    T->left = RotateLeft(T->left);
-    return RotateRight(T);
-}
-
-node* AVL::RL(node* T) {
-    T->right = RotateRight(T->right);
-    return RotateLeft(T);
-}
-
-// Insert with safe balancing
-node* AVL::insert(node* temp, string str_w, string str_m) {
-    if (temp == NULL) {
-        temp = new node;
-        temp->word = str_w;
-        temp->meaning = str_m;
-        temp->left = temp->right = NULL;
-    } else {
-        if (str_w.compare(temp->word) > 0) {
-            temp->right = insert(temp->right, str_w, str_m);
-            if (BF(temp) == -2) {
-                if (temp->right && str_w.compare(temp->right->word) > 0)
-                    temp = RR(temp);
-                else
-                    temp = RL(temp);
-            }
-        } else {
-            temp->left = insert(temp->left, str_w, str_m);
-            if (BF(temp) == 2) {
-                if (temp->left && str_w.compare(temp->left->word) < 0)
-                    temp = LL(temp);
-                else
-                    temp = LR(temp);
-            }
-        }
-    }
-    temp->ht = height(temp);
-    return temp;
-}
-
-// Find minimum node
-node* AVL::findMin(node* root) {
-    while (root->left != NULL)
-        root = root->left;
-    return root;
-}
-
-// Delete node
-node* AVL::deleteNode(node* root, string str_w) {
-    if (root == NULL)
-        return NULL;
-
-    if (str_w < root->word)
-        root->left = deleteNode(root->left, str_w);
-    else if (str_w > root->word)
-        root->right = deleteNode(root->right, str_w);
-    else {
-        if (root->left == NULL) {
-            node* temp = root->right;
-            delete root;
-            return temp;
-        } else if (root->right == NULL) {
-            node* temp = root->left;
-            delete root;
-            return temp;
-        } else {
-            node* temp = findMin(root->right);
-            root->word = temp->word;
-            root->meaning = temp->meaning;
-            root->right = deleteNode(root->right, temp->word);
-        }
-    }
-    root->ht = height(root);
-    return root;
-}
-
-// Display preorder
-void AVL::preorder(node* root) {
-    if (root != NULL) {
-        cout << root->word << " : " << root->meaning << " (Bf=" << BF(root) << ")\n";
-        preorder(root->left);
-        preorder(root->right);
-    }
-}
-
-// Display inorder
-void AVL::inorder(node* root) {
-    if (root != NULL) {
-        inorder(root->left);
-        cout << root->word << " : " << root->meaning << " (Bf=" << BF(root) << ")\n";
-        inorder(root->right);
-    }
-}
-
-// Search
-void AVL::search(node* root, string str_w) {
-    if (root == NULL) {
-        cout << "Word not found" << endl;
-        return;
-    }
-
-    if (str_w.compare(root->word) < 0)
-        search(root->left, str_w);
-    else if (str_w.compare(root->word) > 0)
-        search(root->right, str_w);
-    else {
-        cout << "Word: " << root->word << "\nMeaning: " << root->meaning << endl;
-    }
-}
-
-// Modify
-void AVL::modify(node* root, string str_w) {
-    if (root == NULL) {
-        cout << "Word not found" << endl;
-        return;
-    }
-    if (str_w.compare(root->word) < 0)
-        modify(root->left, str_w);
-    else if (str_w.compare(root->word) > 0)
-        modify(root->right, str_w);
-    else {
-        cout << "Enter new meaning: ";
-        cin.ignore();
-        getline(cin, root->meaning);
-    }
-}
-
-// Main driver
 int main() {
     AVL Tree;
     int ch;
